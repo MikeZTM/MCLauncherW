dcaf0d6ea62d1f984b7347d44de6e81312ecfc55
Mike Lee <liyingzhuomike@gmail.com>
2013-01-08 22:45:35 +0800

    Initial Commit

---
+++ MCLauncherW/MainWindow.xaml.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MCLauncherW
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        private String javaVM;
+        private String playerName;
+        private String playerPswd;
+        private bool x64mode;
+        private long memory;
+        private bool pswdEnabled;
+
+        public MainWindow()
+        {
+            InitializeComponent();
+            refreshSettings();
+        }
+
+        private void start_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void preference_Click(object sender, RoutedEventArgs e)
+        {
+            this.Hide();
+            Preference prefer = new Preference();
+            prefer.setParent(this);
+            prefer.Show();
+        }
+
+        public void refreshSettings()
+        {
+            javaVM = Properties.Settings.Default.javaVM;
+            playerName = Properties.Settings.Default.playerName;
+            playerPswd = Properties.Settings.Default.playerPswd;
+            x64mode=Properties.Settings.Default.x64mode;
+            memory=Properties.Settings.Default.memory;
+            pswdEnabled=Properties.Settings.Default.pswdEnabled;
+        }
+    }
+}
---
+++ MCLauncherW/Preference.xaml.cs
@@ -0,0 +1,131 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Shapes;
+
+namespace MCLauncherW
+{
+    /// <summary>
+    /// Interaction logic for Preference.xaml
+    /// </summary>
+    public partial class Preference : Window
+    {
+        private MainWindow win;
+        private String javaVM;
+        private String playerName;
+        private String playerPswd;
+        private bool x64mode;
+        private long memory;
+        private bool pswdEnabled;
+
+        public Preference()
+        {
+            InitializeComponent();
+            refreshSettings();
+            javaw.Text = javaVM;
+            if (Environment.Is64BitOperatingSystem) //check x64 os
+            {
+                x64Check.IsEnabled = true;
+            }
+            else
+            {
+
+            }
+        }
+
+        private void closed(object sender, EventArgs e)
+        {
+            win.Show();
+        }
+
+        public void setParent(MainWindow win)
+        {
+            this.win = win;
+        }
+
+        private void browse_Click(object sender, RoutedEventArgs e)
+        {
+            Microsoft.Win32.OpenFileDialog dlg = new Microsoft.Win32.OpenFileDialog();
+            dlg.FileName = "javaw.exe"; // Default file name 
+            dlg.DefaultExt = ".exe"; // Default file extension 
+            dlg.Filter = "Executable Binary (.exe)|*.exe"; // Filter files by extension
+
+            Nullable<bool> result = dlg.ShowDialog(); //show dialog
+
+            if (result == true)
+            {
+                // Open document 
+                javaw.Text = dlg.FileName;
+            }
+        }
+
+        private void javaw_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            Properties.Settings.Default.javaVM = javaw.Text;
+            Properties.Settings.Default.Save();
+        }
+
+        public void refreshSettings()
+        {
+            javaVM = Properties.Settings.Default.javaVM;
+            playerName = Properties.Settings.Default.playerName;
+            playerPswd = Properties.Settings.Default.playerPswd;
+            x64mode = Properties.Settings.Default.x64mode;
+            memory = Properties.Settings.Default.memory;
+            pswdEnabled = Properties.Settings.Default.pswdEnabled;
+        }
+
+        private void browseMC_Click(object sender, RoutedEventArgs e)
+        {
+            Microsoft.Win32.OpenFileDialog dlg = new Microsoft.Win32.OpenFileDialog();
+            dlg.FileName = "minecraft.jar"; // Default file name 
+            dlg.DefaultExt = ".jar"; // Default file extension 
+            dlg.Filter = "Java runnable (.jar)|*.jar"; // Filter files by extension
+
+            Nullable<bool> result = dlg.ShowDialog(); //show dialog
+
+            if (result == true)
+            {
+                // Open document 
+                mc_path.Text = dlg.FileName;
+            }
+        }
+
+        private void x64Check_Checked(object sender, RoutedEventArgs e)
+        {
+            memSlider.Maximum = 4096;
+            Properties.Settings.Default.x64mode = true;
+            Properties.Settings.Default.Save();
+        }
+
+        private void x64Check_Unchecked(object sender, RoutedEventArgs e)
+        {
+            memSlider.Maximum = 1536;
+            Properties.Settings.Default.x64mode = false;
+            Properties.Settings.Default.Save();
+        }
+
+        private void memSlider_ValueChanged(object sender, RoutedPropertyChangedEventArgs<double> e)
+        {
+            if (memAmountTextField != null)
+            {
+                memAmountTextField.Content = memSlider.Value;
+            }
+        }
+
+        private void mcpath_TextChanged(object sender, TextChangedEventArgs e)
+        {
+            Properties.Settings.Default.mcPath = mc_path.Text;
+            Properties.Settings.Default.Save();
+        }
+    }
+}
